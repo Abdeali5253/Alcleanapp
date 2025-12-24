6eca28130e2a2d71150ca6166ea3f2dd0c4620ac
Abdeali5253 <abdeali.hazari@gmail.com>
2025-12-24 13:33:15 +0500

    `Removed NotificationPrompt component and replaced with a simple return null statement`

--- FIXES/NotificationPrompt.tsx
+++
@@ -1,8 +0,0 @@
-// Notification prompt disabled for better UX
-// Users will get notifications automatically when they grant permission
-// No need for manual setup prompt
-
-export function NotificationPrompt() {
-  // Return null to hide the notification banner completely
-  return null;
-}
